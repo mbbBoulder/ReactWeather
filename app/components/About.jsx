fc515143d988fc02a03694e878c0f5d13b4237f2
Scott McLamb <scott.mclamb@gmail.com>
2017-01-12 21:19:25 -0700

    Style about page and add link.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,17 @@ var React = require('react');
 // only have render method so:
 var About = (props) => {
   return (
-    <h3>About component</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather app.
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - The JavaScript framework used.
+        </li>
+      </ul>
+    </div>
   )
 };
 
