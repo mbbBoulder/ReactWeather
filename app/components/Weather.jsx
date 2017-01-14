651afaec23c986482431494fb6e0ecf68fbce2a3
Scott McLamb <scott.mclamb@gmail.com>
2017-01-14 10:12:54 -0700

    Lots of styling

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var WeatherMessage = require('WeatherMessage');
+var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
@@ -11,37 +12,50 @@ var Weather = React.createClass({
   },
   handleSearch: function (location) {
     var that = this;
+
     this.setState({
-      isLoading: true
+      isLoading: true,
+      errorMessage: undefined
     });
+
     openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
         location: location,
         temp: temp,
         isLoading: false
       });
-    }, function (errorMessage) {
+    }, function (e) {
       that.setState({
-        isLoading: false
+        isLoading: false,
+        errorMessage: e.message
       });
-      alert(errorMessage);
     });
   },
   render: function () {
-    var {temp, location, isLoading} = this.state;
+    var {isLoading, temp, location, errorMessage} = this.state;
+
     function renderMessage () {
       if (isLoading) {
         return <h3 className="text-center">Fetching weather...</h3>;
       } else if (temp && location) {
-        return <WeatherMessage location={location} temp={temp}/>;
+        return <WeatherMessage temp={temp} location={location}/>;
+      }
+    }
+
+    function renderError () {
+      if (typeof errorMessage === 'string') {
+        return (
+          <ErrorModal message={errorMessage}/>
+        )
       }
     }
 
     return (
       <div>
-        <h1 className="text-center">Get Weather</h1>
+        <h1 className="text-center page-title">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
+        {renderError()}
       </div>
     )
   }
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -10,17 +10,16 @@ var WeatherForm = React.createClass({
       this.refs.location.value = '';
       this.props.onSearch(location);
     }
-
   },
   render: function () {
     return (
-      <div>
-        <form onSubmit={this.onFormSubmit}>
-          <input type="text" ref="location"/>
-          <button className="button expanded hollow">Get Weather</button>
-        </form>
-      </div>
-    )
+        <div>
+          <form onSubmit={this.onFormSubmit}>
+            <input type="search" ref="location" placeholder="Search weather by city"/>
+            <button className="button expanded hollow">Get Weather</button>
+          </form>
+        </div>
+    );
   }
 });
 
